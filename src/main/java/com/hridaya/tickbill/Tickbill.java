b1475013afb37839dd4276d3e88c187cea6d2749
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2024-10-13 11:39:08 +0545

    Initial commit

---
+++ src/main/java/com/hridaya/tickbill/Tickbill.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hridaya.tickbill;
+
+/**
+ *
+ * @author hridaya
+ */
+public class Tickbill {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
