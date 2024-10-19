f2f0052aa365aaaa444cdbd734621ae59945d46c
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2024-10-19 12:44:23 +0545

    Login: Make username and password case-sensitive

--- src/main/java/com/hridaya/tickbill/view/LoginUI.java
+++ src/main/java/com/hridaya/tickbill/view/LoginUI.java
@@ -32,10 +32,12 @@ public class LoginUI extends javax.swing.JFrame {
     private void userLogin() {
         String username = userNameTextField.getText();
         String password = new String(passwordTextField.getPassword());
+        String loginSql = "SELECT * FROM user WHERE " +
+                "BINARY username = ? AND BINARY password = ?";
         if (username != null) {
 
             try (PreparedStatement stmt
-                    = DbConnection.getConnection().prepareStatement("SELECT * FROM user WHERE username = ? AND password = ?")) {
+                    = DbConnection.getConnection().prepareStatement(loginSql)) {
 
                 stmt.setString(1, username);
                 stmt.setString(2, password);
