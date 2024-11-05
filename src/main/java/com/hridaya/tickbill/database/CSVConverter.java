kawaaii/Tickbill

commit f3d3c9dae3a93363a10d97a03ec6757ad1dfbe95

ImportExportUI: Don't create new connection everytime

src/main/java/com/hridaya/tickbill/database/CSVConverter.java
@@ -10,7 +10,6 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,11 +17,10 @@ import java.sql.SQLException;
 public class CSVConverter {
 
     private boolean exportFile(String sqlQuery, String csvFilePath) {
-        try (Connection conn = DbConnection.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sqlQuery);
-             ResultSet rs = stmt.executeQuery();
-             BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath));
-             CSVPrinter printer = new CSVPrinter(writer, CSVFormat.EXCEL.withHeader(rs))) {
+        try (PreparedStatement stmt = DbConnection.getConnection().prepareStatement(sqlQuery)) {
+            ResultSet rs = stmt.executeQuery();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath));
+            CSVPrinter printer = new CSVPrinter(writer, CSVFormat.EXCEL.withHeader(rs));
 
             printer.printRecords(rs);
             return true;
@@ -40,10 +38,9 @@ public class CSVConverter {
     }
 
     private boolean importFile(String sqlQuery, String csvFilePath) {
-        try (Connection conn = DbConnection.getConnection();
-             FileReader reader = new FileReader(csvFilePath);
-             CSVParser parser = new CSVParser(reader, CSVFormat.EXCEL.withHeader());
-             PreparedStatement stmt = conn.prepareStatement(sqlQuery)) {
+        try (PreparedStatement stmt = DbConnection.getConnection().prepareStatement(sqlQuery)) {
+            FileReader reader = new FileReader(csvFilePath);
+            CSVParser parser = new CSVParser(reader, CSVFormat.EXCEL.withHeader());
 
             for (CSVRecord record : parser) {
                 for (int i = 0; i < record.size(); i++) {

pom.xml
@@ -1,5 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
     <modelVersion>4.0.0</modelVersion>
     <groupId>com.hridaya</groupId>
     <artifactId>tickbill</artifactId>

commit 8ddace4168cd2a354fa82406b4636f28f5183ea1

Fixup import/export for real
close resources using try() {}
remove unwanted stmt close

Fixes:  f3d3c9dae3a93363a10d97a03ec6757ad1dfbe95 ImportExportUI: Don't create new connection everytime

src/main/java/com/hridaya/tickbill/database/CSVConverter.java
@@ -17,11 +17,10 @@ import java.sql.SQLException;
 public class CSVConverter {
 
     private boolean exportFile(String sqlQuery, String csvFilePath) {
-        try (PreparedStatement stmt = DbConnection.getConnection().prepareStatement(sqlQuery)) {
-            ResultSet rs = stmt.executeQuery();
-            BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath));
-            CSVPrinter printer = new CSVPrinter(writer, CSVFormat.EXCEL.withHeader(rs));
-
+        try (PreparedStatement stmt = DbConnection.getConnection().prepareStatement(sqlQuery);
+             BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath));
+             ResultSet rs = stmt.executeQuery();
+             CSVPrinter printer = new CSVPrinter(writer, CSVFormat.EXCEL.withHeader(rs))) {
             printer.printRecords(rs);
             return true;
         } catch (IOException | SQLException ex) {
@@ -38,8 +37,8 @@ public class CSVConverter {
     }
 
     private boolean importFile(String sqlQuery, String csvFilePath) {
-        try (PreparedStatement stmt = DbConnection.getConnection().prepareStatement(sqlQuery)) {
-            FileReader reader = new FileReader(csvFilePath);
+        try (PreparedStatement stmt = DbConnection.getConnection().prepareStatement(sqlQuery);
+             FileReader reader = new FileReader(csvFilePath)) {
             CSVParser parser = new CSVParser(reader, CSVFormat.EXCEL.withHeader());
 
             for (CSVRecord record : parser) {

src/main/java/com/hridaya/tickbill/view/ImportExportUI.java
@@ -234,7 +234,6 @@ public class ImportExportUI extends javax.swing.JPanel {
                         // delete existing data from user table
                         try (Statement st = DbConnection.getConnection().createStatement()) {
                             st.executeUpdate("DELETE FROM user");
-                            st.close();
                         } catch (SQLException e) {
                             Utils.showError(e.getMessage());
                         }
@@ -246,7 +245,6 @@ public class ImportExportUI extends javax.swing.JPanel {
                         // delete existing data from sales_history table
                         try (Statement st = DbConnection.getConnection().createStatement()) {
                             st.executeUpdate("DELETE FROM sales_history");
-                            st.close();
                         } catch (SQLException e) {
                             Utils.showError(e.getMessage());
                         }
@@ -258,7 +256,6 @@ public class ImportExportUI extends javax.swing.JPanel {
                         // delete existing data from inventory table
                         try (Statement st = DbConnection.getConnection().createStatement()) {
                             st.executeUpdate("DELETE FROM inventory");
-                            st.close();
                         } catch (SQLException e) {
                             Utils.showError(e.getMessage());
                         }